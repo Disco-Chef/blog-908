07918903eceac0edf01fb0ea929debf10b83d733
Nicolas Agathos <nicolas.agathos@gmail.com>
2022-05-20 10:31:46 +0200

    wow photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
+file = URI.open('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fannapinkerton.com%2Fwp-content%2Fuploads%2F2018%2F08%2Fbusiness-workshop-placeholder-1024x768.jpg&f=1&nofb=1')
+article = Article.new(title: 'Communicating is hard...', body: "We all know that having to work with humans is not always the best....")
+article.photo.attach(io: file, filename: 'business.jpg', content_type: 'image/jpg')
+article.save
